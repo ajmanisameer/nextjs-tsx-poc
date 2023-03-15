71d73ebd5bdde24e578942aa1d7fa94ac5724986
Sameer Ajmani <47604025+ajmanisameer@users.noreply.github.com>
2023-03-15 12:23:08 +0530

    Auth0 introduced

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,17 +2,19 @@ import '../styles/global.css';
 
 import type { AppProps } from 'next/app';
 import Script from 'next/script';
+import { UserProvider } from '@auth0/nextjs-auth0/client';
 
 const MyApp = ({ Component, pageProps }: AppProps) => (
   <>
-    <Script
-      id="my-script"
-      strategy="lazyOnload"
-      src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
-    />
+    <UserProvider>
+      <Script
+        id="my-script"
+        strategy="lazyOnload"
+        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
+      />
 
-    <Script id="my-script" strategy="lazyOnload">
-      {`
+      <Script id="my-script" strategy="lazyOnload">
+        {`
         window.dataLayer = window.dataLayer || [];
         function gtag(){dataLayer.push(arguments);}
         gtag('js', new Date());
@@ -20,8 +22,9 @@ const MyApp = ({ Component, pageProps }: AppProps) => (
         page_path: window.location.pathname,
         });
     `}
-    </Script>
-    <Component {...pageProps} />
+      </Script>
+      <Component {...pageProps} />
+    </UserProvider>
   </>
 );
 
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,23 +1,32 @@
 // import { useRouter } from 'next/router';
-
+import { useUser } from '@auth0/nextjs-auth0/client';
 import { Meta } from '@/layouts/Meta';
 import { Main } from '@/templates/Main';
 
 const Index = () => {
- // const router = useRouter();
+  // const router = useRouter();
+  const { user, error, isLoading } = useUser();
+  if (isLoading) return <div>Loading...</div>;
+  if (error) return <div>{error.message}</div>;
 
-  return (
-    <Main
-      meta={
-        <Meta
-          title="POC"
-          description="Description"
-        />
-      }
-    >
-      
-    </Main>
-  );
+  if (user) {
+
+    return (
+      <Main
+        meta={
+          <Meta
+            title="POC"
+            description="Description"
+          />
+        }
+      >
+        Welcome {user.name}! <a href="/api/auth/logout">Logout</a>
+        <h1>asdsadas</h1>
+
+      </Main>
+    );
+  }
+
 };
 
 export default Index;
